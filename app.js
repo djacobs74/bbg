ab391ae8eabcd3f41fb643c93b0ccad57c1799c2
David Jacobs <djacobs74@gmail.com>
2018-07-11 00:05:25 -0700

    button conditions set

--- app.js
+++ app.js
@@ -7,14 +7,17 @@ new Vue({
 		zombieType: 'None',
 		room: 1,
 		betaComponents: 0,
+		searchChance: '',
 	},
 	methods: {
 		phillipAttack: function() {
-			var damage = this.calculateDamage(20, 40);
-			this.enemyHealth -= damage;
-			this.enemyAttacks();
-			this.turnCounter();
-			this.zombieDead();
+			if (this.zombieType != 'None') {
+				var damage = this.calculateDamage(20, 40);
+				this.enemyHealth -= damage;
+				this.enemyAttacks();
+				this.turnCounter();
+				this.zombieDead();
+			}
 		},
 		enemyAttacks: function() {
 			var damage = this.calculateDamage(3, 10);
@@ -32,14 +35,16 @@ new Vue({
 
         },
         phillipHeal: function() {
-        	var heal = this.calculateHeal(4, 10);
-        	this.playerHealth += heal;
-        	if (this.playerHealth > 100) {
-        		this.playerHealth = 100;
-        	} else {
-        		this.playerHealth;
-        	}
-        	this.turnCounter();
+        	if (this.zombieType == 'None' && this.playerHealth < 100) {
+	        	var heal = this.calculateHeal(4, 10);
+	        	this.playerHealth += heal;
+	        	if (this.playerHealth > 100) {
+	        		this.playerHealth = 100;
+	        	} else {
+	        		this.playerHealth;
+	        	}
+	        	this.turnCounter();
+	        }
         },
         numberGenerator: function(min, max) {
         	return Math.max(Math.floor(Math.random() * max) + 1, min);
@@ -49,23 +54,26 @@ new Vue({
         	var zombieType = '';
         	var zombieHealth = '';
 
-        	result = this.numberGenerator(1, 12);
-        	if (result >= 1 && result <= 3) {
-        		zombieType = 'Alpha';
-        		zombieHealth = 50;
-        	} else if (result >= 3 && result <= 6) {
-        		zombieType = 'Beta';
-        		zombieHealth = 75;
-        	} else if (result >= 7 && result <= 9) {
-        		zombieType = 'Zed';
-        		zombieHealth = 100;
-        	} else {
-        		zombieType = 'None';
-        	}
-        	this.zombieType = zombieType;
-        	this.enemyHealth = zombieHealth;
-        	this.room += 1;
-        	this.turnCounter();
+        	if (this.zombieType === 'None') {
+
+	        	result = this.numberGenerator(1, 12);
+	        	if (result >= 1 && result <= 3) {
+	        		zombieType = 'Alpha';
+	        		zombieHealth = 50;
+	        	} else if (result >= 3 && result <= 6) {
+	        		zombieType = 'Beta';
+	        		zombieHealth = 75;
+	        	} else if (result >= 7 && result <= 9) {
+	        		zombieType = 'Zed';
+	        		zombieHealth = 100;
+	        	} else {
+	        		zombieType = 'None';
+	        	}
+	        	this.zombieType = zombieType;
+	        	this.enemyHealth = zombieHealth;
+	        	this.room += 1;
+	        	this.turnCounter();
+	        }
         },
         zombieDead: function() {
         	if (this.enemyHealth <= 0) {
@@ -73,10 +81,13 @@ new Vue({
         	}
 		},
 		searchRoom: function() {
-			result = this.numberGenerator(1, 12);
-			if (result >= 1 && result <= 3) {
-				this.betaComponents += 1;
-			} 
+			if (this.zombieType == 'None' && this.room != 1) {
+				result = this.numberGenerator(1, 12);
+				if (result >= 1 && result <= 3) {
+					this.betaComponents += 1;
+				} 
+				this.turnCounter();
+			}
 		}
 	},
 
